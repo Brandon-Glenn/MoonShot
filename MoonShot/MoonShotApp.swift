5bb39288223c6886ca430e2a643de48d74cc9a5c
Brandon Glenn <Brandon.Glenn@icloud.com>
2021-12-09 09:11:04 -0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Brandon Glenn on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
